d77e79a7ca01e720763cf4f24b228361affd1979
andigiovan <giovanandi@yahoo.com>
2019-10-17 15:50:52 +0700

    moment js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,13 +2,13 @@ const db = require("../database")
 
 module.exports = {
     getComment: (req, res) =>{
-        db.query(`select * from comments where articleid = ${req.query.articleid}`, (err, result) => {
+        db.query(`select * from comments where articleid = ${req.query.articleid} order by created_at desc`, (err, result) => {
             if (err) throw err
             res.send(result)
         })
     }, 
     addComment: (req, res) => {
-        db.query(`insert into comments values (0, "${req.body.username}" , "${req.body.comment}", ${req.body.articleid})`, (err, result) => {
+        db.query(`insert into comments values (0, "${req.body.username}" , "${req.body.comment}", ${req.body.articleid}, "${req.body.created_at}" )`, (err, result) => {
             if (err) throw err
             res.send(result)
             console.log(result);
